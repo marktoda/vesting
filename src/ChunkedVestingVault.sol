19e8a57cf3169d74188ef2a7bb91006fbf733699
Mark Toda <toda.mark@gmail.com>
2022-08-10 12:03:29 -0700

    feat: update pragma to ^0.8.0

--- src/ChunkedVestingVault.sol
+++ src/ChunkedVestingVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.0;
 
 import {IERC20Upgradeable} from
     "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
